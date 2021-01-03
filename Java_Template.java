32582be3b0c4646ad786e517be8a61234ab9129a
Dawson Teu <dawsonteu@gmail.com>
2021-01-03 00:32:53 -0500

    Update Java template with file reading

--- Java_Template.java
+++ Java_Template.java
@@ -1,13 +1,27 @@
 import java.util.*;
 import java.io.*;
 
-public class Test {
+public class Java_Template {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello, World!");
+        /*
+         For fast input from a text file
+         br = new BufferedReader(new FileReader("./src/put_file_here.txt"));
+         System.out.println(br.readLine());
+         br.close();
+         */
+
+         pr.println("Hello, World!");
+         long l = readLong();
+         int i = readInt();
+         double d = readDouble();
+         char c = readCharacter();
+         String s = readLine();
+         System.out.println(l + i + d);
+         System.out.println(c + s);
     }
 
     static String next() throws IOException {
