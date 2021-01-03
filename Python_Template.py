1fcec66d7c0c284c387d9181f232913caba6f27b
Dawson <dawsonteu@gmail.com>
2021-01-02 22:03:29 -0500

    Update Python template with file reading

--- Python_Template.py
+++ Python_Template.py
@@ -2,5 +2,11 @@
 # import sys
 # input = sys.stdin.readline
 
+# For fast input from a text file
+# If input is small lines can be buffered or otherwise used in place
+# with open("put_file_here.txt") as f:
+#     for line in f:
+#         print(line)
+
 import sys
 data = sys.stdin.read().split('\n')
