0ca0b05d7f1fa24441be02f3812c34ad043014ff
Dawson <dawsonteu@gmail.com>
2021-01-02 22:10:02 -0500

    Add text file inputs for AoC 2019 and update Day 1 to read from file

--- Advent_of_Code/2019/Day_1_The_Tyranny_of_the_Rocket_Equation.py
+++ Advent_of_Code/2019/Day_1_The_Tyranny_of_the_Rocket_Equation.py
@@ -1,12 +1,16 @@
 total_1 = 0
 total_2 = 0
+
+f = open("InputFiles/Day_1.txt", "r")
 for i in range(100):
-    num = int(input())
+    num = int(f.readline())
 
     total_1 += (num // 3) - 2
     while (num // 3 - 2) > 0:
         num //= 3
         num -= 2
         total_2 += num
+f.close()
+
 print(total_1)  # part 1
 print(total_2)  # part 2
